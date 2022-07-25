fc1f7e764945ce9635e5283ca3d2e31123f4c50c
Gioack <101208747+Gioack@users.noreply.github.com>
2022-07-25 15:50:51 +0200

    added default problems

--- main.py
+++ main.py
@@ -13,11 +13,77 @@ app = FastAPI()
 # FIXME: Should only be done once, clears the database
 db.api.initiate_database()
 db.api.insert_question(
-    question='This is a question, pretend it is nicely formatted',
+    question='''def multiplication(a, b):
+    """This function multiplies 2 numbers"""
+    return a*b''',
+    answer='Feature',
+    title='Multiplication',
+    expl='There is not that much to say, * operator multiples 2 numbers!',
+    diff=0)
+db.api.insert_question(
+    question='''def square_of_a_number(a):
+    """This function calculates the square of a number."""
+    return a ^ 2''',
     answer='Bug',
-    title='Question title, hint: the answer is Bug',
-    expl='This is just a placeholder')
+    title='Square_of_a_number',
+    expl='''Almost everyone has made this mistake once! In Python `^` is the[XOR operator]
+("https://docs.python.org/3/reference/expressions.html#binary-bitwise-operations") \
+while the power of a number is represented by `**`.
+# Corrected code
+```
+5 return a ** 2
+```''',
+    diff=0)
+db.api.insert_question(
+    question='''import random
 
+
+def slot_machine():
+    """The following simulates a slot machine with 3 entries.
+
+    Each entry shows a number between 0 and 9.
+    It will always return the numbers rolled and a message.
+    If all the numbers are the same, the message is "Jackpot!",
+    if 2 numbers are the same, the message is "Good roll"" and
+    if they are all different, the message is "Try again, you will be luckier".
+    """
+    digits = range(10)
+    first_n = random.choice(digits)
+    second_n = random.choice(digits)
+    third_n = random.choice(digits)
+    roll = [first_n, second_n, third_n]
+    if __all_numbers_are_the_same(roll):
+        return roll, "Jackpot!"
+    elif __two_numbers_are_the_same(roll):
+        return roll, "Good roll!"
+    else:
+        return roll, "Try again, you will be luckier!"
+
+
+def __all_numbers_are_the_same(numbers):
+    return set(numbers) == 1
+
+
+def __two_numbers_are_the_same(numbers):
+    return set(numbers) == 2
+
+
+print(slot_machine())''',
+    answer='Bug',
+    title='Slot machine',
+    expl='''The first 2 conditional statements will always be False, \
+we are comparing a set and an integer they will always be different!
+The code can be fixed by applying a len() function to the set of each function.
+Now we're comparing 2 integers as it was intended!
+#### Corrected code
+```
+26 def all_numbers_are_the_same(numbers):
+27     return len(set(numbers)) == 1
+28
+29
+30 def two_numbers_are_the_same(numbers):
+31     return len(set(numbers)) == 2
+```''', diff=1)
 
 # FIXME: Should be changed to serve the frontend index.html
 html = """
@@ -251,13 +317,13 @@ def process_new_answer(user, event):
     return ret
 
 
-@app.get("/")
+@ app.get("/")
 async def get_main_page():
     """Returns the main html body"""
     return HTMLResponse(html)
 
 
-@app.get("/solve_quiz")
+@ app.get("/solve_quiz")
 async def get_quiz_page():
     """Returns the quiz html body,
 
@@ -277,7 +343,7 @@ async def get_cookie_or_token(
     return session or token
 
 
-@app.websocket("/solve_quiz/{item_id}/ws")
+@ app.websocket("/solve_quiz/{item_id}/ws")
 async def websocket_endpoint_quiz(
     websocket: WebSocket,
     item_id: str,
@@ -313,7 +379,7 @@ async def websocket_endpoint_quiz(
             logger.error('Unrecognized type "%s"', event['type'])
 
 
-@app.websocket("/new_question/{item_id}/ws")
+@ app.websocket("/new_question/{item_id}/ws")
 async def websocket_endpoint(
     websocket: WebSocket,
     item_id: str,
