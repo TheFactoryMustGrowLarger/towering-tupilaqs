b4a43b73308f03e5fa639d7328da01ef2dea7908
Nyxion <raggar_tobba_92@hotmail.com>
2022-07-28 06:19:00 +0200

    fixed some spacing, formatting and made use or more props

--- site/src/App.js
+++ site/src/App.js
@@ -4,24 +4,39 @@ import {BrowserRouter, Link, Route, Routes} from "react-router-dom"
 
 import React, {useEffect, useRef, useState} from 'react';
 
-
+/**
+ * @description Takes a type which correlates to the backend. Available types right now is:
+ *
+ * - 'get_question'
+ * - 'answered_question'
+ * - 'insert_new_question'
+ * - TODO: Needs to be implemented more
+ *  - 'token_pls' - gets token for security
+ * @param {string} type
+ * @param {Object} data
+ * @returns {string}
+ */
 const createMessage = (type, data) => {
+
     return JSON.stringify({ 'type': type, 'data': data});
 }
 
+/**
+ * @description Makes debug messages
+ * @param {string} type
+ * @param {Object} data
+ * @returns {`${string} <> tupilaqs <> DEBUG <> ${string} - ${string}`}
+ */
 const debugMessage = (type, data) => {
     const date = new Date().toUTCString();
     return `${date} <> tupilaqs <> DEBUG <> ${type} - ${JSON.stringify(data)}`;
 }
 
-
-const LandingPage = (props) =>{
-    const { webSocket } = props;
-    const [user_name, set_user_name] = useState("")
-    const [error, set_error] = useState("")
+const LandingPage = ({ webSocket, setUserName, userName}) => {
+    const [error, setError] = useState("")
     const add_new_question = (e) => {
         e.preventDefault();
-        if(user_name){
+        if (userName) {
                const itemId = document.getElementById("itemID").value;
                const new_question_text = document.getElementById("newQuestionText").value;
                const correct_answer = document.getElementById("correctAnswer").value;
@@ -37,25 +52,12 @@ const LandingPage = (props) =>{
                 };
 
 
-                webSocket.send(createMessage('new_question', request))
-        }
-        else{
-            set_error("The user name field must be filled in")
+                webSocket.send(createMessage('insert_new_question', request))
+        } else {
+            setError("The user name field must be filled in")
         }
 
         clearAddQ();
-    }
-
-    const check_start_game = () => {
-        let itemId = user_name
-        if(user_name){
-            const request = {
-                user: itemId
-            }
-        }
-        else{
-            set_error("The username field must be filled in")
-        }
     }
 
     const clearAddQ = () => {
@@ -68,10 +70,10 @@ const LandingPage = (props) =>{
     return (
         <div>
             <div className="inputs registration">
-                <input type="text" value={user_name} onChange={e => set_user_name(e.target.value)} className="input-base" id="itemID" placeholder="Username: "/>
+                <input type="text" value={userName} onChange={e => setUserName(e.target.value)} className="input-base" id="itemID" placeholder="Username: "/>
                  <p className="error">{error}</p>
             </div>
-            <Link to={user_name ? "/categories" : ""}><button className="bb-button start-button" onClick={check_start_game}>New Game</button></Link>
+            <Link to={userName ? "/categories" : ""}><button className="bb-button start-button">New Game</button></Link>
             <h1 className="main-title center">Add custom questions </h1>
             <div className="inputs">
                 <input type="text" className="input-base" id="newQuestionText" placeholder="New Question: "/>
@@ -91,29 +93,76 @@ const LandingPage = (props) =>{
 }
 
 //FIXME: Did not work: function CodeBox({ getQuestion }) {
-const Box = (props) =>{
-    const { webSocket } = props;
+const Box = ({ webSocket, userName, wsMessage, questions, setQuestions }) => {
+    const [isStarted, setStarted] = useState(false);
 
-    const getQuestion = () => {
-        webSocket.send(createMessage('get_question',));
-        // TODO: TO BE CONTINUED
+    useEffect(() => {
+        if (isStarted) return;
+        startGame();
+    }, []);
+
+    /**
+     * For starting game, should probably be expanded upon.
+     */
+    const startGame = () => {
+        const data = {
+            'user_name': userName
+        }
+        webSocket?.send(createMessage('get_question', data));
+        setStarted(true);
+    }
+
+    /**
+     * @description Sends the guess of the player to backend to verify.
+     * @param {Event} e
+     * @param {string} guess
+     */
+    const handleGuess = (e, guess) => {
+        e.preventDefault();
+        if (!questions.answer) return;
+
+        if (questions.answer.toString().toLowerCase() === guess) {
+            console.log("You got it correct!!");
+            // Now it needs to get a new question, answer
+            // Add it to the users correct_answers in DB
+        } else {
+            console.log("THAT'S WRONG YOU NERD, HA!");
+        }
     }
 
     return (
          <div className="box">
+
+                <div>
+                    <h1 className="main-title">{questions.title ? questions.title : 'title'}</h1>
+                </div>
+                <div>
+                    <h4 style={{maxWidth: '550px', color: '#FFC0CB'}}>
+                        {questions.expl ? questions.expl : 'explanation'}
+                    </h4>
+                    <br />
+                    <h4 style={{color: '#FFC0CB'}}>
+                        Difficulty: {questions.difficulty ? questions.difficulty : 'difficulty'}
+                    </h4>
+                    <h4 style={{maxWidth: '550px', color: '#FFC0CB'}}>
+                        Votes: {questions.votes ? questions.votes : 'votes'}
+                    </h4>
+                </div>
                 <div className="code-snippet">
-                    <MyCoolCodeBlock code={getQuestion} language={"python"}/>
+                    <MyCoolCodeBlock code={questions.txt ? questions.txt : ''} language={"python"}/>
                 </div>
                 <div className="buttons">
-                    <button className="bb-button bug">
+                    <button className="bb-button bug" onClick={(e) => handleGuess(e, 'bug')}>
                         Bug
                     </button>
-                    <button className="bb-button feature">
-                        Features
+                    <button className="bb-button feature" onClick={(e) => handleGuess(e, 'feature')}>
+                        Feature
                     </button>
-                    <button className="bb-button upload">
-                        Upload my own question!
-                    </button>
+                    <Link to="/">
+                        <button className="bb-button upload">
+                            Upload my own question!
+                        </button>
+                    </Link>
                 </div>
           </div>
 
@@ -132,29 +181,32 @@ function MyCoolCodeBlock({ code, language }) {
 }
 
 function App() {
-    // Tried to define this useState at this level, and then pass the getQuestion reference down to the CodeBox function
-    //const [getQuestion, setQuestion] = useState("print('hello world')");
+    // Tried to define this useState at this level, and then pass the getQuestion reference
+    // down to the CodeBox function
+    // const [getQuestion, setQuestion] = useState("print('hello world')");
     //FIXME:Did not work: <Route path="/categories" element={CodeBox(getQuestion)}/>
 
-    const [socketURL, setSocketURL] = useState('ws://localhost:8000/quiz');
-    const [token, setToken] = useState('');
-    const [wsMessage, setWSMessage] = useState(null);
+    const socketURL = useRef('ws://localhost:8000/quiz');
     const ws = useRef(null);
+    const [token, setToken] = useState('');
+    const [wsMessage, setWSMessage] = useState('');
+    const [questions, setQuestions] = useState([]);
+    const [userName, setUserName] = useState('');
 
     // Only runs when connection is open and closed.
     useEffect(() => {
-        ws.current = new WebSocket(socketURL);
+        ws.current = new WebSocket(socketURL.current);
 
         ws.current.onopen = () => {
-            console.log('Websocket opened. URL: ', socketURL);
-            ws.current?.send(JSON.stringify({'type': 'token_pls'}))
+            console.log('Websocket opened. URL: ', socketURL.current);
+            ws.current?.send(JSON.stringify({'type': 'token_pls'}));
         }
         ws.current.onclose = () => {
-            console.log('Websocket closed. URL: ', socketURL);
+            console.log('Websocket closed. URL: ', socketURL.current);
         }
 
         ws.current.onmessage = (event) => {
-            const j_obj = JSON.parse(event.data)
+            const j_obj = JSON.parse(event.data);
             const type = j_obj.type;
             const data = j_obj.data;
             switch (type) {
@@ -164,6 +216,10 @@ function App() {
                 case 'return':
                     console.log(debugMessage(type, data));
                     setWSMessage(data);
+                    break;
+                case 'serve_question':
+                    console.log(debugMessage(type, data));
+                    setQuestions(data);
                     break;
                 default:
                     console.log(debugMessage(type, data));
@@ -175,14 +231,23 @@ function App() {
         };
     }, []);
 
+    const propsPackage = {
+        webSocket: ws.current,
+        userName: userName,
+        setUserName: setUserName,
+        wsMessage: wsMessage,
+        questions: questions,
+        setQuestions: setQuestions,
+    }
+
     return (
 	<BrowserRouter>
         <div className="App">
             <h1 className="main-title">WebSocket Quiz - Bug, Feature or Tupilaqs</h1>
-
+            <h1 className="main-title">{wsMessage}</h1>
             <Routes>
-                <Route path="/" element={<LandingPage webSocket={ws.current}/>}/>
-                <Route path="/categories" element={<Box webSocket={ws.current} wsMessage={wsMessage}/>} />
+                <Route path="/" element={<LandingPage{...propsPackage}/>}/>
+                <Route path="/categories" element={<Box {...propsPackage} />}/>
             </Routes>
 	    </div>
 	</BrowserRouter>
