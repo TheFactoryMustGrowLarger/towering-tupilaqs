7e8d8bf5bd3636de6f7265e3445b1e69adeac1c3
TheFactoryMustGrowLarger <ob+thefactorymustgrowlarger@cakebox.net>
2022-07-29 11:57:02 +0200

    added as_list helper function to reduce copy paste

--- db/utils/db_classes.py
+++ db/utils/db_classes.py
@@ -50,11 +50,29 @@ class User:
     correct_answers: str
     incorrect_answers: str
     submitted_questions: str
+    submitted_add_votes: str
     ident: str
 
 
+class UserList:
+    """Base class for the different lists that the User table contains"""
+
+    def as_list(self, property_name):
+        """Helper function to split the given property to a list of strings
+
+        assumes seperation by ', ' and excludes empty items
+        """
+        value = getattr(self, property_name, None)
+        if value is None:
+            return list()
+
+        sp = value.split(', ')
+        empty_removed = filter(lambda x: x != '', sp)
+        return list(empty_removed)
+
+
 @dataclass
-class UserCA:
+class UserCA(UserList):
     """User correct answers Type Struct"""
 
     correct_answers: str
@@ -72,3 +90,10 @@ class UserSQ:
     """User incorrect answers Type Struct"""
 
     submitted_questions: str
+
+
+@dataclass
+class UserSV:
+    """User incorrect answers Type Struct"""
+
+    submitted_add_votes: str
