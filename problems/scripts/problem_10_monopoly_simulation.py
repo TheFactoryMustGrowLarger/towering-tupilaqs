7c5584b3074e7f14771015392b4391033ab38175
Gioack <101208747+Gioack@users.noreply.github.com>
2022-07-31 16:32:51 +0200

    small adjustments to problem 10

--- problems/scripts/problem_10_monopoly_simulation.py
+++ problems/scripts/problem_10_monopoly_simulation.py
@@ -2,7 +2,34 @@ from itertools import product
 from random import choice
 
 
-class __monopoly_probabilities():
+class MonopolyProbabilities():
+    """It calculates the probability of finishing at a particular land after a roll through a Montecarlo simulation.
+
+    Monopoly has 40 squares called lands (we enumerate them starting from 0),
+    and the players move around them using two dice.
+    You can find a Monopoly board at this link, it will be helpful:
+    https://c8.alamy.com/comp/BF7EFX/monopoly-board-game-BF7EFX.jpg
+
+    Without extra variables, the task of this function would be easy if Monopoly didn't have some rules that
+    make this harder:
+    When you go on land 30 you always finish at land 10.
+    When you go on Community Chest(lands 2, 17, 23) you draw a card, 2/16 cards will move you:
+        1.  Advance to GO
+        2.  Go to JAIL
+    When you go on Community Chest you draw a card, 10/16 cards will move you:
+        1.  Advance to GO
+        2.  Go to JAIL
+        3.  Go to C1
+        4.  Go to E3
+        5.  Go to H2
+        6.  Go to R1
+        7.  Go to next R (railway company)
+        8.  Go to next R
+        9.  Go to next U (utility company)
+        10. Go back 3 squares.
+    These are the only rules that this function considers.
+    """
+
     def __init__(self):
         self.__make_dice_possibilities()
         self.appearances_of_each_land = {key: 0 for key in range(40)}
@@ -12,33 +39,8 @@ class __monopoly_probabilities():
         all_combination_of_two_dices = list(product(all_faces_of_dice, all_faces_of_dice))
         self.dice_possibilities = list(map(sum, all_combination_of_two_dices))
 
-    def main(self):
-        """It calculates the probability of finishing at a particular land after a roll through a Montecarlo simulation.
-
-        Monopoly has 40 squares called lands (we enumerate them starting from 0),
-        and the players move around them using two dice.
-        You can find a Monopoly board at this link, it will be helpful:
-        https://c8.alamy.com/comp/BF7EFX/monopoly-board-game-BF7EFX.jpg
-
-        Without extra variables, the task of this function would be easy if Monopoly didn't have some rules that
-        make this harder:
-        When you go on land 30 you always finish at land 10.
-        When you go on Community Chest(lands 2, 17, 23) you draw a card, 2/16 cards will move you:
-            1.  Advance to GO
-            2.  Go to JAIL
-        When you go on Community Chest you draw a card, 10/16 cards will move you:
-            1.  Advance to GO
-            2.  Go to JAIL
-            3.  Go to C1
-            4.  Go to E3
-            5.  Go to H2
-            6.  Go to R1
-            7.  Go to next R (railway company)
-            8.  Go to next R
-            9.  Go to next U (utility company)
-            10. Go back 3 squares.
-        These are the only rules that this function considers.
-        """
+    def __call__(self):
+        """It returns probabilities"""
         self.__run_simulation()
         probabilities = self.__make_probabilities()
         return probabilities
@@ -73,3 +75,6 @@ class __monopoly_probabilities():
         for land, appearances in self.appearances_of_each_land.items():
             probabilities[land] = f"{round((appearances/10**6)*100, 2)}%"
         return probabilities
+
+
+print(MonopolyProbabilities()())
