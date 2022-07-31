9c758b0015df5e09f72c15a96c6cc23125077881
Nyxion <raggar_tobba_92@hotmail.com>
2022-07-31 16:18:11 +0200

    changed so .log file can only go up to 5mb with 2 backups

--- utilites/logger_utility.py
+++ utilites/logger_utility.py
@@ -1,4 +1,5 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
 
 def setup_logger():
@@ -6,7 +7,9 @@ def setup_logger():
     logger = logging.getLogger('tupilaqs')
     logger.setLevel(logging.DEBUG)
 
-    ch = logging.FileHandler('tupilaqs.log')
+    log_file = 'tupilaqs.log'
+    ch = RotatingFileHandler(log_file, mode='a', maxBytes=5*1024*1024,
+                             backupCount=2, encoding=None, delay=False)
     ch.setLevel(logging.DEBUG)
     # create formatter
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
